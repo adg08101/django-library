2f7f6cc79f240962c0c0e15ff6f37c30843b4ed3
klol <adg08101@gmail.com>
2021-05-24 14:04:51 -0400

    Views Commit
    New views on app Templates

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -13,13 +13,15 @@ def index(request):
     # Libros disponibles (status = 'a')
     num_instances_available = BookInstance.objects.filter(status__exact='a').count()
     num_authors = Author.objects.count()  # El 'all()' esta implícito por defecto.
+    num_genres = Genre.objects.count()
 
     # Renderiza la plantilla HTML index.html con los datos en la variable contexto
     return render(
         request,
         'index.html',
         context={'num_books': num_books, 'num_instances': num_instances,
-                 'num_instances_available': num_instances_available, 'num_authors': num_authors},
+                 'num_instances_available': num_instances_available, 'num_authors': num_authors,
+                 'num_genres': num_genres},
     )
 
 
