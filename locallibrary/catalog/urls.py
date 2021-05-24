8ffb3a0af03f012156adb9037d3d56c1d2391abb
klol <adg08101@gmail.com>
2021-05-24 12:57:43 -0400

    Views Commit
    New views on app Templates

--- locallibrary/catalog/urls.py
+++ locallibrary/catalog/urls.py
@@ -5,4 +5,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'books', views.books, name='books'),
+    url(r'authors', views.authors, name='authors'),
 ]
--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -21,3 +21,33 @@ def index(request):
         context={'num_books': num_books, 'num_instances': num_instances,
                  'num_instances_available': num_instances_available, 'num_authors': num_authors},
     )
+
+
+def authors(request):
+    """
+    Función vista para la página de autores del sitio.
+    """
+
+    authors = Author.objects.all()  # El 'all()' esta implícito por defecto.
+
+    # Renderiza la plantilla HTML index.html con los datos en la variable contexto
+    return render(
+        request,
+        'authors.html',
+        context={'authors': authors},
+    )
+
+
+def books(request):
+    """
+    Función vista para la página de libros del sitio.
+    """
+
+    books = Book.objects.all()  # El 'all()' esta implícito por defecto.
+
+    # Renderiza la plantilla HTML index.html con los datos en la variable contexto
+    return render(
+        request,
+        'books.html',
+        context={'books': books},
+    )
