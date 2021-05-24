9c85b767871c2043fb587a2144c97fc3f3b27422
klol <adg08101@gmail.com>
2021-05-24 11:21:11 -0400

    Views Commit
    New views on app

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -1,7 +1,23 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Book, Author, BookInstance, Genre
 
 
-# Create your views here.
 def index(request):
-    return HttpResponse('Hello library')
+    """
+    Función vista para la página inicio del sitio.
+    """
+    # Genera contadores de algunos de los objetos principales
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+    # Libros disponibles (status = 'a')
+    num_instances_available = BookInstance.objects.filter(status__exact='a').count()
+    num_authors = Author.objects.count()  # El 'all()' esta implícito por defecto.
+
+    # Renderiza la plantilla HTML index.html con los datos en la variable contexto
+    return render(
+        request,
+        'index.html',
+        context={'num_books': num_books, 'num_instances': num_instances,
+                 'num_instances_available': num_instances_available, 'num_authors': num_authors},
+    )
